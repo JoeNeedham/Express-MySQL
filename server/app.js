5c1570b3973c9b2e1d6eb0ccb8bc04750e136050
joeneedum <joeneedum@gmail.com>
2022-08-21 20:28:49 -0400

    commit

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const dbService = require('./dbService')
+
 // allows request from the frontend
 app.use(cors());
 // express to recognize the incoming Request Object as a JSON Object. 
@@ -13,12 +15,24 @@ app.use(express.urlencoded({ extended: false }))
 
 // create
 app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
 
+    const result = db.insertNewName(name);
+
+    result.then(data => response.json({ success: true}))
+    .catch(err => console.log(err));
 });
 
 // read
 app.get('/getAll', (request, response) => {
-    console.log('test');
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
 });
 
 // delete
